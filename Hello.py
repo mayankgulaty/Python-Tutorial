4d1def315f575ce6659e1ee7a19a2e0e1ecb3032
Mayank Gulaty <mayankgulaty@gmail.com>
2017-03-05 21:09:07 +0000

    Basics

---
+++ Hello.py
@@ -0,0 +1,19 @@
+# take input
+
+greeting = "Hello"
+name = input("Please enter your name:\n")
+print(greeting + ' ' + name)
+
+print('''test
+splitting into lines
+test
+''')
+
+a = 20
+b = 10
+
+print (a / b)  # prints 2.0, returns float
+
+print (a // b) # prints 2, returns int
+
+
